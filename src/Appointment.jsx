4b35ee9fca71dfb17871962b1ad906c75fca106e
Koshal765 <koshalpothare@gamil.com>
2025-10-20 18:05:02 +0530

    Added delete and edit functionallity

--- src/Appointment.jsx
+++ src/Appointment.jsx
@@ -10,7 +10,17 @@ const Appointment = () => {
     date: '',
     time: ''
   })
+
+  const reset = ()=>{
+    setData({
+    name: '',
+    service: '',
+    date: '',
+    time: ''
+    })
+  }
   const [Appointment, SetAppointments] = useState([])
+   const [editIndex,SeteditIndex] = useState( null)
 
   useEffect(() => {
     const StoredAppointments = JSON.parse(localStorage.getItem("data")) || [];
@@ -22,17 +32,50 @@ const Appointment = () => {
   }
 
   const handleSubmit = (e) => {
-    e.preventDefault();
-    notify()
+    e.preventDefault(); 
     const StoredAppointments = JSON.parse(localStorage.getItem("data")) || [];
-    const updated = [...StoredAppointments, data]
-    localStorage.setItem("data", JSON.stringify(updated));
-    SetAppointments(updated)
-    console.log(data);
+
+    if(editIndex !== null){
+   
+   StoredAppointments[editIndex]= data
+   localStorage.setItem("data", JSON.stringify(StoredAppointments));
+    SetAppointments(StoredAppointments)
+    SeteditIndex(null);
+    console.log(data); 
+    editnotify()
+    }
+    else{
+  const updated = [...StoredAppointments, data];
+  localStorage.setItem("data",JSON.stringify(updated))
+  SetAppointments(updated);
+  notify();
+    }
     
+    reset();
   }
 
-  const notify = () => toast("Appointmnet Booked!")
+  const Delete = (index)=>{
+    if(window.confirm("Do you want to delete this?")){
+         const StoredAppointments = JSON.parse(localStorage.getItem("data")) || [];
+         StoredAppointments.splice(index,1)
+         localStorage.setItem("data",JSON.stringify(StoredAppointments))
+         SetAppointments(StoredAppointments);
+         deletenotify(); 
+  }  
+   
+  }
+
+const Edit =(index)=>{
+     const StoredAppointments = JSON.parse(localStorage.getItem("data"))||[];
+     const selected= StoredAppointments[index];
+     setData(selected)
+     SeteditIndex(index);
+}
+
+
+  const notify = () => toast.success("Appointmnet Saved Succesfully")
+  const deletenotify=()=> toast.success("Appointment Deleted Succesfully")
+  const editnotify=()=>toast.info("Appointment Updated Succesfully")
 
   return (
     <>
@@ -43,14 +86,14 @@ const Appointment = () => {
           <div className='mt-3'>
             <label>Enter Your Name</label>
             <div>
-              <input type='text' name='name' placeholder='eg:amit' value={data.name} onChange={handleChange} className='border rounded-lg lg:w-100 md:w-80 sm:w-50 px-2 py-1  focus:border-emerald-400   outline-none ' required></input>
+              <input type='text' name='name' placeholder='eg:amit' value={data.name} onChange={handleChange} className='border rounded-lg lg:w-100 md:w-80 sm:w-50 px-2 py-1  focus:border-emerald-400   outline-none ' required autoComplete='off'></input>
             </div></div>
 
 
           <div className='mt-3'>
             <label>Select Service</label>
             <div>
-              <input type='text' name='service' value={data.service} onChange={handleChange} placeholder='eg:haircut,gym,doctor,etc'  className='border-1 rounded-lg lg:w-100 md:w-80 sm:w-50 px-2 py-1 focus:border-emerald-400   outline-none' required></input>
+              <input type='text' name='service' value={data.service} onChange={handleChange} placeholder='eg:haircut,gym,doctor,etc'  className='border-1 rounded-lg lg:w-100 md:w-80 sm:w-50 px-2 py-1 focus:border-emerald-400   outline-none' required autoComplete='off'></input>
             </div></div>
 
 
@@ -70,11 +113,8 @@ const Appointment = () => {
 
 
           <div className='flex justify-center mt-3'>
-          <button type='submit'  className='bg-emerald-700 text-white font-semibold px-2 py-2 rounded-2xl  mt-3 lg:w-100 md:w-80 sm:w-50 hover:bg-emerald-600 hover:text-lg'>Save Appointment</button>
-          <ToastContainer
-            autoClose={3000}
-            theme='dark'
-            transition={Slide} />
+          <button type='submit'  className='bg-emerald-700 text-white font-semibold px-2 py-2 rounded-2xl  mt-3 lg:w-100 md:w-80 sm:w-50 hover:bg-emerald-600 hover:text-lg'>{editIndex !== null ?"Update Appointment" : "Save Appointment"}</button>
+          
             </div>
 
         </form>
@@ -86,7 +126,7 @@ const Appointment = () => {
     <div className='p-5 m-5 shadow-xl/30 rounded-lg w-fu h-auto bg-gray-50'>
     <h1 className='lg:text-2xl md:text-xl sm:text-lg font-semibold text-emerald-600 font-serif'>Appointmnets</h1>
     <div className='overflow-x-auto'>
-    <table className='min-w-full mt-5 border-seperate border-2 border-gray-400 md:table-auto sm: col-span-2 '> 
+    <table className='min-w-full mt-5 border-separate border-2 border-gray-400 md:table-auto sm: col-span-2 '> 
         <thead>
             <tr className='border-2 border-gray-500'>
                 <th className='border border-gray-300 px-4 py-2 text-xl'>Name</th>
@@ -107,8 +147,8 @@ const Appointment = () => {
                     <td className='border border-gray-300 px-4 py-2 text-center'>{app.time}</td>
                     <td className='border border-gray-300 px-4 py-2 '>
                       <div className=' flex justify-around'>
-                        <button className='bg-red-500 text-white font-semibold px-3 py-1 rounded-2xl hover:bg-red-700'>Delete</button>
-                    <button className=' bg-emerald-600 text-white font-semibold px-4 py-1 rounded-2xl hover:bg-emerald-400'>Edit</button>
+                        <button className='bg-red-500 text-white font-semibold px-3 py-1 rounded-2xl hover:bg-red-700'onClick={()=>Delete(index)}>Delete</button>
+                    <button className=' bg-emerald-600 text-white font-semibold px-4 py-1 rounded-2xl hover:bg-emerald-400' onClick={()=>Edit(index)}>Edit</button>
                     </div>
                     </td>
                    </tr>
@@ -119,6 +159,7 @@ const Appointment = () => {
 
     </div>
 </div>
+<ToastContainer autoClose={2000} theme='dark' transition={Slide} />
     </>
   )
 }
