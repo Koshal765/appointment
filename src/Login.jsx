1361696f8372825918efe50d5549d425e04245e9
Koshal765 <koshalpothare@gamil.com>
2025-10-25 02:05:30 +0530

    added functionallity to login and sign up

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,13 +1,20 @@
 import React, { useState } from 'react'
+import { useNavigate } from 'react-router';
+import { toast, ToastContainer,Slide} from 'react-toastify';
+import Swal from 'sweetalert2';
 
 const Login = () => {
-  const[isLogin,setIsLogin] = useState(true);
+  const[isLogin,setIsLogin] = useState(false);
   const[user,setUser]=useState({
     username:'',
     password:""
   })
 
-  
+  const[showpass,setShowPass] = useState(true);
+
+
+  const notify = () =>toast.info("Username Already Exists")
+  const navigate = useNavigate()
 
 const handleChange =(e)=>{
   setUser({...user,[e.target.name]:e.target.value})
@@ -15,18 +22,41 @@ const handleChange =(e)=>{
 const handleSubmit=(e)=>{
   e.preventDefault();
   const userdata = JSON.parse(localStorage.getItem("users")) || [];
-  const matchedUser = userdata.find(u => u.username === user.username && u.password === user.password);
+  if(isLogin){
+  const matchedUser = userdata.find(u => u.username.trim().toLowerCase() === user.username.trim().toLowerCase() && u.password.trim().toLowerCase() === user.password.trim().toLowerCase());
   if(matchedUser){
-    alert("Login Successful");
+      Swal.fire({
+      title: "Login Successful!",
+      text: `Welcome ${matchedUser.username}!`,
+      icon: "success",
+      confirmButtonColor: "#10b981" // emerald color
+    });
+    navigate('/');
   }
   else{
     alert("Invalid Credentials");
+  }}
+  else{
+    const exists = userdata.some((u)=>u.username=== user.username);
+    if(exists){
+      notify()
+    }
+    else{
+      userdata.push(user)
+      localStorage.setItem("users",JSON.stringify(userdata));
+ Swal.fire({
+      title: "Signup Successful!",
+      text: "You can now login.",
+      icon: "success",
+      confirmButtonColor: "#10b981"
+    });
+      setIsLogin(true);
+    }
+
   }
-
-
-  
+setUser({username:'' , password:''});
   console.log(user);
-}
+};
 
   return (
     <>
@@ -44,15 +74,15 @@ const handleSubmit=(e)=>{
          
           {/* <label className=' font-serif'>Password</label> */}
           <div className='mt-5'>
-            <input name='password' type='password' value={user.password} onChange={handleChange} required autoComplete='off' className='border rounded-full w-full focus:border-emerald-400   outline-none px-3 py-1' placeholder='password'></input>
+            <input name='password' type='text' value={user.password} onChange={handleChange} required autoComplete='off' className='border rounded-full w-full focus:border-emerald-400   outline-none px-3 py-1' placeholder='password'></input>
           </div>
         
        
         <div className='flex justify-center mt-8'>
           <button className='bg-gradient-to-r from-emerald-500 via-teal-500 to-green-400 px-4 py-2 font-serif text-center rounded-full w-full text-white ' type='submit'>Login</button>
           </div>
-           <div className='mt-5 w-full  flex items-center'>
-     <p className='text-md'>Create Account ?</p><span className=' text-blue-500 hover:text-blue-700'> <a href='#' onClick={()=>setIsLogin(false)} className='cursor-pointer text-md'>Sign Up</a></span>
+           <div className='mt-5 w-full  flex items-center justify-center gap-1'>
+     <p className='text-md'>Create Account?</p><span className=' text-blue-500 hover:text-blue-700'> <a href='#' onClick={()=>setIsLogin(false)} className='cursor-pointer text-sm'>Sign Up</a></span>
         </div>
       </form>
       </div>
@@ -76,7 +106,7 @@ const handleSubmit=(e)=>{
         <div className='flex justify-center mt-8'>
           <button className='bg-gradient-to-r from-emerald-500 via-teal-500 to-green-400 px-4 py-2 font-serif text-center rounded-full w-full text-white ' type='submit'>Sign Up</button>
           </div>
-                <div className='mt-5 w-full  flex justify-center items-center'>
+                <div className='mt-5 w-full  flex justify-center items-center gap-1'>
  <p className='text-md'>Have an Account?</p><span className=' text-blue-500 hover:text-blue-700'> <a href='#' onClick={()=>setIsLogin(true)} className='cursor-pointer text-md'>Login</a></span>
         </div>
       </form>
@@ -85,6 +115,10 @@ const handleSubmit=(e)=>{
       
       </>}
       </div>
+      <ToastContainer 
+      autoClose={2000}
+      transition={Slide}
+      theme='dark'/>
     </>
   )
 }
